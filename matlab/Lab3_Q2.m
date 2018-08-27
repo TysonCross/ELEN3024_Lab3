a82dceb1e64b02eb5834368ab8bec538c192862a
Tyson <tyson.cross@gmail.com>
2018-08-27 23:41:53 +0200

    QI demodulation of DSB-FC complete

--- matlab/Lab3_Q2.m
+++ matlab/Lab3_Q2.m
@@ -27,45 +27,17 @@ tm = t*1e3;
 N = numel(t);                               %  number of samples
 f = linspace(-f_s/2,f_s/2,N);               %  frequency range
 
-%% Equations
-%{
-message_I = A_m * cos(2 * pi * f_m * t );
-message_Q = A_m * sin(2 * pi * f_m * t );
-% message_Q = imag(hilbert(message_I));
-
-carrier_I = A_c * cos(2 * pi * f_c * t);
-carrier_Q = A_c * sin(2 * pi * f_c * t);
-
-message = message_I - message_Q;
-carrier = carrier_I - carrier_Q;
-
-%  IQ DSB-SC:
-modulated_signal_I = (1 + message_I) .* carrier_I;
-modulated_signal_Q = (1 + message_Q) .* carrier_Q;
-modulated_signal = modulated_signal_I - modulated_signal_Q;
-
-% Demodulation
-magnitude_signal = abs(modulated_signal);
-% compensate for MATLAB complex signal magnitude:
-% filtered_signal = (1/sqrt(2)).*lowpass(magnitude_signal,1,f_m)-0.2;
-filtered_signal = lowpass(magnitude_signal,1,f_m);
-demodulated_signal = 2*filtered_signal - 2;
-
-% Envelope:
-envelope1 = message_I + 1;
-envelope2 = -envelope1;
-%}
-
-%% Equations
-message = A_m * cos(2 * pi * f_m * t);
-carrier = A_c * cos(2 * pi * f_c * t);
+%% Equations (normal DSB-FC as per Lab 1)
+phase = pi/4;
+message = A_m * cos(2 * pi * f_m * t + phase);
+carrier = A_c * cos(2 * pi * f_c * t + phase);
 
 % DSB-SC modulation
 modulated_signal = (1 + message) .* carrier;
 
-% Demodulation
+% Demodulation (Using a QI structure)
 local_oscillator_I = cos(2 * pi * f_c * t);
-local_oscillator_Q = imag(hilbert(local_oscillator_I));
+local_oscillator_Q = imag(hilbert(local_oscillator_I)); % 90 degree out of phase
 
 demod_I = modulated_signal.*local_oscillator_I;
 demod_Q = modulated_signal.*local_oscillator_Q;
@@ -73,13 +45,17 @@ demod_Q = modulated_signal.*local_oscillator_Q;
 filtered_signal_I = lowpass(demod_I,1,f_m);
 filtered_signal_Q = lowpass(demod_Q,1,f_m);
 
-envelope = sqrt(acos(filtered_signal_I).^2 + asin(filtered_signal_Q).^2);
-phase = atan(asin(filtered_signal_Q)/acos(filtered_signal_I));
+envelope = sqrt(filtered_signal_I.^2 + filtered_signal_Q.^2);
+phase_recovered = atan2(filtered_signal_Q,filtered_signal_I);
+
+% Restore the amplitude and remove the DC offset from the demodulated signal:
+demodulated_signal = 2*(envelope) - 1;
 
 % Frequency
 message_frequency =     (A_m/N)  * abs(fftshift((fft(message))));
 modulated_frequency =   (A_c*A_m/N)  * abs(fftshift((fft(modulated_signal))));
-demodulated_frequency = (A_m/N)  * abs(fftshift((fft(filtered_signal_I-mean(filtered_signal_I)))));
+% remove the DC offset from the demodulated signal for the FFT:
+demodulated_frequency = (A_m/N)  * abs(fftshift((fft(demodulated_signal-mean(demodulated_signal)))));
 
 f_message = [-f_m 0 f_m];
 f_output = [-f_c-f_m -f_c+f_m 0 f_c-f_m f_c+f_m];
